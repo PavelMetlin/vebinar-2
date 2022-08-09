943302bc050fd92901553210792f8ff01cba4d14
PavelMetlin <metlin-1@mail.ru>
2022-08-09 15:13:26 +0300

    tasc3

--- tasc3/Program.cs
+++ tasc3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek(int dayNum)
+{
+    if (dayNum == 6 || dayNum == 7)
+    {
+        Console.WriteLine("(этот день выходной) -> да");
+    }
+    else if (dayNum < 1 || dayNum > 7)
+    {
+        Console.WriteLine("это не день недели");
+    }
+    else Console.WriteLine("(этот день не выходной) -> нет");
+}
+
+CheckingTheDayOfTheWeek(dayNum);
